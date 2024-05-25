649a0096e789008476a72fef2f75dd859be02b16
rinkuJat <jatr26008@gmail.com>
2024-05-25 13:11:34 +0530

    Changed host

--- frontend/src/context/notes/NoteState.js
+++ frontend/src/context/notes/NoteState.js
@@ -3,7 +3,7 @@ import { useState } from "react";
 import noteContext from "./noteContext";
 
 const NoteState = (props) => {
-    const host = "http://localhost:5000";
+    const host = "https://my-notebook-chi.vercel.app";
     const notesInitial = [];
     const [notes, setNotes] = useState(notesInitial);
 
